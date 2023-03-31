daced6e6518aa8ba12b1a6992730138a7beb3e07
Fatchul Bari Hikmawan <wawan@kyodo-i.com>
2023-03-31 16:03:20 +0700

    modify Vite to meet best practice deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import purge from '@erbelion/vite-plugin-laravel-purgecss';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,16 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        purge({
+            templates: ['blade','vue']
+        }),
+        viteCompression({
+            algorithm: 'gzip'
+        }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
