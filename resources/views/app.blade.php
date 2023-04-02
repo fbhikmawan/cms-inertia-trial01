9708527d51c698b655ed22fdc2119bca7c8192fc
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2023-04-02 14:25:52 +0700

    fixing dependencies

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,7 +24,7 @@
         @routes
         @vite([
             'resources/js/app.js',
-            "=resources/js/Pages/{$page['component']}.vue"])
+            "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
