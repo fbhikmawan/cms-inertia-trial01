a71c746a3237d71198da65c0ea752a73bbe13b16
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2023-04-02 10:55:50 +0700

    Revert "try to remove hmr"
    This reverts commit c3d1fa3f310e6589ca7283a873227d5acb612ff7.

--- vite.config.js
+++ vite.config.js
@@ -27,5 +27,10 @@ export default defineConfig({
         viteCompression({
             algorithm: 'gzip'
         }),
-    ]
+    ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
